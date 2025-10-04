ade1100b72baa96bee74d8eb9f1e70946fae1489
Bidhan Chhetri <bidhanchhetri52@gmail.com>
2025-10-04 10:06:57 +0545

    reverse commit

--- Functions/reverse_number.c
+++ Functions/reverse_number.c
@@ -11,11 +11,6 @@ void rev() {
     int r,n,rev = 0;
     printf("Enter the number");
     scanf("%d",&n);
-    while(n!=0)
-    {
-        r = n % 10;
-        rev = rev * 10 + r;
-        n = n / 10;
-    }
+   
     printf("\nThe reversed number is %d",rev);
 }
