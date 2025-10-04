433c70ac2247b7231a7ac1c44f392ae484f159de
Bidhan Chhetri <bidhanchhetri52@gmail.com>
2025-10-04 11:08:20 +0545

    add commit

--- Functions/greet.c
+++ Functions/greet.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world! How are you?\n");
+    printf("Hello world! How are you? what are you doiong?\n");
+    printf("I am fine, thank you!\n");
+    printf("Goodbye!\n");
     return 0;
 }
